07804c1c9d4bef253d1c34b0d35da33ddd3e9245
Dee'Kej <deekej@linuxmail.org>
2014-05-16 16:13:58 +0200

    client/mediator: The chasis has been implemented.

--- src/client/client_mediator.cc
+++ src/client/client_mediator.cc
@@ -15,6 +15,9 @@
  ~ ~~~[ HEADER FILES ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~
  * ****************************************************************************************************************** */
 
+#include <cassert>
+#include <cstdio>
+
 #include "client_mediator.hh"
 
 /* ****************************************************************************************************************** *
@@ -27,70 +30,475 @@ namespace client {
   mediator::mediator(client::settings_tuple &settings) :
     interface_barrier_(2), connection_barrier_(2), settings_{settings} 
   {{{
+    // Preparing TCP connection:
     p_tcp_connect_ = new client::tcp_connection(settings_, message_in_, action_req_, action_req_mutex_,
                                                 new_message_flag_, connection_barrier_);
-
+    
+    // Preparing the interface:
     p_interface_ = new client::terminal_interface(action_req_, action_req_mutex_, interface_barrier_, user_command_,
                                                   additional_data_);
     return;
   }}}
 
+  /**
+   * Making sure we terminate all properly.
+   */
   mediator::~mediator()
   {{{
+    p_tcp_connect_->disconnect();
+    p_interface_->terminate();
+
     delete p_tcp_connect_;
     delete p_interface_;
+
     return;
   }}}
 
+  // // // // // // // // // // // //
+
+  /**
+   * Starts the execution of the master logic of the mediator.
+   *
+   * @return  Exit code to be return to the terminal.
+   */
   client::exit_codes mediator::run()
   {{{
+    // Acquire initial lock on the action request mutex:
     boost::unique_lock<boost::mutex> action_lock(action_req_mutex_);
+
     p_interface_->initialize();
-    interface_barrier_.wait();
-
+    interface_barrier_.wait();                // Synchronize with the interface.
+    
+    // Try to connect to server:
     if (p_tcp_connect_->connect() == false) {
-      return client::exit_codes::E_CONNECT_FAILED;
+      display_message_error();
+      return exit_codes::E_CONNECT_FAILED;    // Connection fail, nothing more to do.
     }
 
-    connection_barrier_.wait();
-#if 0   
-    boost::asio::io_service io_service;
-    boost::asio::deadline_timer timer(io_service);
+    connection_barrier_.wait();               // Synchronize with established TCP connection thread.
 
-    for (unsigned i = 0; i < 6; i++) {
-      std::cout << "Going to sleep, iteration #" << i << std::endl;
-      timer.expires_from_now(boost::posix_time::seconds(5));
-      timer.wait();
-    }
+    do {
+      action_req_.wait(action_lock);          // Wait for asynchronous event occurrence.
+      
+      // Call the user command handler, if it's needed:
+      if (user_command_ != command::NONE) {
+        (this->*commands_handlers_[user_command_])();
+      }
+      
+      // Categorize the incoming message and call appropriate handler:
+      if (new_message_flag_ == true) {
+        new_message_flag_ = false;            // Reseting the flag.
+
+        switch (message_in_.type) {
+          case protocol::E_type::CTRL :
+            ctrl_message_handler();
+            break;
+
+          case protocol::E_type::INFO :
+            if (message_in_.info_type >= 0 && message_in_.info_type < E_INFO_TYPE_SIZE) {
+              (this->*info_message_handlers_[message_in_.info_type])();
+            }
+            else {
+              // TODO: send message about wrong protocol
+              display_error("Server is using wrong version of communication protocol, terminating...");
+              retval_ = exit_codes::E_WRONG_PROTOCOL;
+              run_ = false;
+            }
+            break;
+
+          case protocol::E_type::ERROR :
+            error_message_handler();
+            break;
+        }
+      }
+
+    } while (run_ == true);
+
+    return retval_;
+  }}}
+
+  // // // // // // // // // // // //
+
+  /**
+   * Prepares the message to be sent [1st overload]. This is overload for CTRL type of message.
+   *
+   * @param[in]   type        Type of the message to be used. (Used for assertion checking for protocol correctness.)
+   * @param[in]   ctrl_type   Specification of the CTRL message.
+   * @param[in]   status      Status of the message.
+   * @param[in]   data        Additional information of the message, empty by default.
+   */
+  inline void mediator::message_prepare(protocol::E_type type, protocol::E_ctrl_type ctrl_type,
+                                        protocol::E_status status, std::vector<std::string> data)
+  {{{
+    assert(type == protocol::E_type::CTRL);       // Make sure we're sending the right type of message.
+
+    message_out_.type = type;
+    message_out_.ctrl_type = ctrl_type;
+    message_out_.status = status;
+    message_out_.data = data;
+
+    return;
+  }}}
+
+#if 0
+  /**
+   * Prepares the message to be sent [2nd overload]. This is overload for INFO type of message.
+   *
+   * @param[in]   type        Type of the message to be used. (Used for assertion checking for protocol correctness.)
+   * @param[in]   info_type   Specification of the INFO message.
+   * @param[in]   status      Status of the message.
+   * @param[in]   data        Additional information of the message, empty by default.
+   */
+  inline void mediator::message_prepare(protocol::E_type type, protocol::E_info_type info_type,
+                                        protocol::E_status status, std::vector<std::string> data)
+  {{{
+    assert(type == protocol::E_type::INFO);       // Make sure we're sending the right type of message.
+
+    message_out_.type = type;
+    message_out_.info_type = info_type;
+    message_out_.status = status;
+    message_out_.data = data;
+
+    return;
+  }}}
 #endif
 
-    while (run_ == true) {
-      action_req_.wait(action_lock);
+  /**
+   * Prepares the message to be sent [3rd overload]. This is overload for ERROR type of message.
+   *
+   * @param[in]   type        Type of the message to be used. (Used for assertion checking for protocol correctness.)
+   * @param[in]   error_type  Specification of the ERROR message.
+   * @param[in]   status      Status of the message.
+   * @param[in]   data        Additional information of the message, empty by default.
+   */
+  inline void mediator::message_prepare(protocol::E_type type, protocol::E_error_type error_type,
+                                        protocol::E_status status, std::vector<std::string> data)
+  {{{
+    assert(type == protocol::E_type::ERROR);      // Make sure we're sending the right type of message.
 
-      if (user_command_ != command::NONE) {
-        if (user_command_ == command::EXIT) {
-          run_ = false;
-          p_interface_->display_message("exiting");
-        }
-        else {
-          p_interface_->display_message("user input");
-        }
-      }
+    message_out_.type = type;
+    message_out_.error_type = error_type;
+    message_out_.status = status;
+    message_out_.data = data;
 
-      if (new_message_flag_ == true) {
-        p_interface_->display_message("new message");
-        std::cout << message_in_.type << message_in_.ctrl_type << message_in_.status << message_in_.data[0] << std::endl;
-      }
+    return;
+  }}}
+
+  // // // // // // // // // // // //
+  
+  /**
+   * TODO: This is dummy for now.
+   */
+  void mediator::ctrl_message_handler()
+  {{{
+    return;
+  }}}
+
+
+  /**
+   * TODO: This is dummy for now.
+   */
+  void mediator::error_message_handler()
+  {{{
+    return;
+  }}}
+
+  // // // // // // // // // // // //
+
+  /**
+   * Displays the error messages generated from server or locally during network communication.
+   */
+  void mediator::display_message_error()
+  {{{
+    assert(p_interface_ != NULL);
+
+    std::string type;
+    
+    // Setting the right format:
+    if (message_in_.status == protocol::E_status::LOCAL) {
+      type = "ERROR: ";
+    }
+    else {
+      type = "Server ERROR: ";
+    }
+    
+    // There might be more than one error message:
+    for (auto msg : message_in_.data) {
+      p_interface_->display_message(type + msg);
     }
 
-    p_tcp_connect_->disconnect();
-    std::cerr << "disconnect passed" << std::endl;
-    p_interface_->terminate();
-    std::cerr << "terminate passed" << std::endl;
+    return;
+  }}}
 
-    return client::exit_codes::NO_ERROR;
+
+  /**
+   * Displays single string as an error message:
+   */
+  void mediator::display_error(const std::string &str)
+  {{{
+    assert(p_interface_ != NULL);
+
+    p_interface_->display_message("ERROR: " + str);
+
+    return;
+  }}}
+
+  // // // // // // // // // // // //
+
+  void mediator::CMD_NONE_handler()
+  {{{
+    p_interface_->display_message("Command not implemented yet, sorry.");
+
+    return;
+  }}}
+
+
+  void mediator::CMD_LEFT_handler()
+  {{{
+    p_interface_->display_message("Command not implemented yet, sorry.");
+
+    return;
+  }}}
+
+
+  void mediator::CMD_RIGHT_handler()
+  {{{
+    p_interface_->display_message("Command not implemented yet, sorry.");
+
+    return;
+  }}}
+
+
+  void mediator::CMD_UP_handler()
+  {{{
+    p_interface_->display_message("Command not implemented yet, sorry.");
+
+    return;
+  }}}
+
+
+  void mediator::CMD_DOWN_handler()
+  {{{
+    p_interface_->display_message("Command not implemented yet, sorry.");
+
+    return;
+  }}}
+
+
+  void mediator::CMD_STOP_handler()
+  {{{
+    p_interface_->display_message("Command not implemented yet, sorry.");
+
+    return;
+  }}}
+
+
+  void mediator::CMD_TAKE_OPEN_handler()
+  {{{
+    p_interface_->display_message("Command not implemented yet, sorry.");
+
+    return;
+  }}}
+
+
+  void mediator::CMD_PAUSE_CONTINUE_handler()
+  {{{
+    p_interface_->display_message("Command not implemented yet, sorry.");
+
+    return;
+  }}}
+
+
+  void mediator::CMD_LIST_MAZES_handler()
+  {{{
+    p_interface_->display_message("Command not implemented yet, sorry.");
+
+    return;
+  }}}
+
+
+  void mediator::CMD_LIST_SAVES_handler()
+  {{{
+    p_interface_->display_message("Command not implemented yet, sorry.");
+
+    return;
+  }}}
+
+
+  void mediator::CMD_LIST_RUNNING_handler()
+  {{{
+    p_interface_->display_message("Command not implemented yet, sorry.");
+
+    return;
+  }}}
+
+
+  void mediator::CMD_GAME_START_handler()
+  {{{
+    p_interface_->display_message("Command not implemented yet, sorry.");
+
+    return;
+  }}}
+
+
+  void mediator::CMD_GAME_RESTART_handler()
+  {{{
+    p_interface_->display_message("Command not implemented yet, sorry.");
+
+    return;
+  }}}
+
+
+  void mediator::CMD_GAME_TERMINATE_handler()
+  {{{
+    p_interface_->display_message("Command not implemented yet, sorry.");
+
+    return;
+  }}}
+
+
+  void mediator::CMD_GAME_JOIN_handler()
+  {{{
+    p_interface_->display_message("Command not implemented yet, sorry.");
+
+    return;
+  }}}
+
+
+  void mediator::CMD_GAME_LEAVE_handler()
+  {{{
+    p_interface_->display_message("Command not implemented yet, sorry.");
+
+    return;
+  }}}
+
+
+  void mediator::CMD_GAME_LOAD_LAST_handler()
+  {{{
+    p_interface_->display_message("Command not implemented yet, sorry.");
+
+    return;
+  }}}
+
+
+  void mediator::CMD_GAME_LOAD_handler()
+  {{{
+    p_interface_->display_message("Command not implemented yet, sorry.");
+
+    return;
+  }}}
+
+
+  void mediator::CMD_GAME_SAVE_handler()
+  {{{
+    p_interface_->display_message("Command not implemented yet, sorry.");
+
+    return;
+  }}}
+
+
+  void mediator::CMD_GAME_SHOW_STATS_handler()
+  {{{
+    p_interface_->display_message("Command not implemented yet, sorry.");
+
+    return;
+  }}}
+
+
+  void mediator::CMD_SET_NICK_handler()
+  {{{
+    p_interface_->display_message("Command not implemented yet, sorry.");
+
+    return;
+  }}}
+
+
+  void mediator::CMD_EXIT_handler()
+  {{{
+    // TODO: Terminate other necessary things.
+    run_ = false;
+
+    return;
+  }}}
+
+
+  void mediator::CMD_HELP_handler()
+  {{{
+    p_interface_->display_message("Command not implemented yet, sorry.");
+
+    return;
+  }}}
+
+
+  void mediator::CMD_ERROR_INPUT_STREAM_handler()
+  {{{
+    p_interface_->display_message("Command not implemented yet, sorry.");
+
+    return;
+  }}}
+
+  // // // // // // // // // // // //
+
+  void mediator::MSG_HELLO_handler()
+  {{{
+    return;
+  }}}
+
+
+  void mediator::MSG_LOAD_DATA_handler()
+  {{{
+    return;
+  }}}
+
+
+  void mediator::MSG_GAMEs_DATA_handler()
+  {{{
+    return;
+  }}}
+
+
+  void mediator::MSG_PLAYER_JOINED_handler()
+  {{{
+    return;
+  }}}
+
+
+  void mediator::MSG_PLAYER_LEFT_handler()
+  {{{
+    return;
+  }}}
+
+
+  void mediator::MSG_PLAYER_KILLED_handler()
+  {{{
+    return;
+  }}}
+
+
+  void mediator::MSG_PLAYER_GAME_OVER_handler()
+  {{{
+    return;
+  }}}
+
+
+  void mediator::MSG_PLAYER_WIN_handler()
+  {{{
+    return;
+  }}}
+
+
+  void mediator::MSG_GAME_RESTARTED_handler()
+  {{{
+    return;
+  }}}
+
+
+  void mediator::MSG_GAME_TERMINATED_handler()
+  {{{
+    return;
   }}}
 }
+
 
 /* ****************************************************************************************************************** *
  * ***[ END OF CLIENT_MEDIATOR.CC ]********************************************************************************** *
--- src/client/client_mediator.hh
+++ src/client/client_mediator.hh
@@ -30,6 +30,7 @@
 /* ****************************************************************************************************************** *
  ~ ~~~[ MEDIATOR CLASS ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~
  * ****************************************************************************************************************** */
+using command = ABC::user_interface::E_user_command;
 
 namespace client {
 
@@ -41,11 +42,9 @@ namespace client {
    */
   class mediator {
       client::tcp_connection                      *p_tcp_connect_ {NULL};
-      // client::udp_connection                *p_udp_connect_ {NULL};
+      // client::udp_connection                      *p_udp_connect_ {NULL};
 
       ABC::user_interface                         *p_interface_ {NULL};
-      enum ABC::user_interface::E_user_command    user_command_;
-      std::string                                 additional_data_;
 
       protocol::message                           message_in_;
       protocol::message                           message_out_;
@@ -53,13 +52,120 @@ namespace client {
       boost::condition_variable                   action_req_;
       boost::mutex                                action_req_mutex_;
 
-      bool                                        new_message_flag_ {false};
-      bool                                        run_ {true};
-
       boost::barrier                              interface_barrier_;
       boost::barrier                              connection_barrier_;
 
       client::settings_tuple                      &settings_;
+
+      command                                     user_command_ {command::NONE};
+      std::string                                 additional_data_;
+      client::exit_codes                          retval_ {exit_codes::NO_ERROR};
+
+      bool                                        new_message_flag_ {false};
+      bool                                        run_ {true};
+
+      // // // // // // // // // // //
+
+      using pf_input_handler = void (mediator::*)();
+
+      pf_input_handler                            commands_handlers_[ABC::user_interface::USER_COMMANDS_SIZE] {
+        &mediator::CMD_NONE_handler,
+        &mediator::CMD_LEFT_handler,
+        &mediator::CMD_RIGHT_handler,
+        &mediator::CMD_UP_handler,
+        &mediator::CMD_DOWN_handler,
+        &mediator::CMD_STOP_handler,
+        &mediator::CMD_TAKE_OPEN_handler,
+        &mediator::CMD_PAUSE_CONTINUE_handler,
+        &mediator::CMD_LIST_MAZES_handler,
+        &mediator::CMD_LIST_SAVES_handler,
+        &mediator::CMD_LIST_RUNNING_handler,
+        &mediator::CMD_GAME_START_handler,
+        &mediator::CMD_GAME_RESTART_handler,
+        &mediator::CMD_GAME_TERMINATE_handler,
+        &mediator::CMD_GAME_JOIN_handler,
+        &mediator::CMD_GAME_LEAVE_handler,
+        &mediator::CMD_GAME_LOAD_LAST_handler,
+        &mediator::CMD_GAME_LOAD_handler,
+        &mediator::CMD_GAME_SAVE_handler,
+        &mediator::CMD_GAME_SHOW_STATS_handler,
+        &mediator::CMD_SET_NICK_handler,
+        &mediator::CMD_EXIT_handler,
+        &mediator::CMD_HELP_handler,
+        &mediator::CMD_ERROR_INPUT_STREAM_handler,
+      };
+
+      pf_input_handler                            info_message_handlers_[E_INFO_TYPE_SIZE]{
+        &mediator::MSG_HELLO_handler,
+        &mediator::MSG_LOAD_DATA_handler,
+        &mediator::MSG_GAMEs_DATA_handler,
+        &mediator::MSG_PLAYER_JOINED_handler,
+        &mediator::MSG_PLAYER_LEFT_handler,
+        &mediator::MSG_PLAYER_KILLED_handler,
+        &mediator::MSG_PLAYER_GAME_OVER_handler,
+        &mediator::MSG_PLAYER_WIN_handler,
+        &mediator::MSG_GAME_RESTARTED_handler,
+        &mediator::MSG_GAME_TERMINATED_handler,
+      };
+
+      // // // // // // // // // // //
+
+      inline void message_prepare(protocol::E_type type, protocol::E_ctrl_type ctrl_type, protocol::E_status status,
+                                  std::vector<std::string> data = {""});
+      // inline void message_prepare(protocol::E_type type, protocol::E_info_type info_type, protocol::E_status status,
+      //                             std::vector<std::string> data = {""});
+      inline void message_prepare(protocol::E_type type, protocol::E_error_type error_type, protocol::E_status status,
+                                  std::vector<std::string> data = {""});
+
+      // // // // // // // // // // //
+
+      void ctrl_message_handler();
+      void error_message_handler();
+
+      // // // // // // // // // // //
+      
+      void display_message_error();
+      void display_error(const std::string &str);
+
+      // // // // // // // // // // //
+
+      void CMD_NONE_handler();
+      void CMD_LEFT_handler();
+      void CMD_RIGHT_handler();
+      void CMD_UP_handler();
+      void CMD_DOWN_handler();
+      void CMD_STOP_handler();
+      void CMD_TAKE_OPEN_handler();
+      void CMD_PAUSE_CONTINUE_handler();
+      void CMD_LIST_MAZES_handler();
+      void CMD_LIST_SAVES_handler();
+      void CMD_LIST_RUNNING_handler();
+      void CMD_GAME_START_handler();
+      void CMD_GAME_RESTART_handler();
+      void CMD_GAME_TERMINATE_handler();
+      void CMD_GAME_JOIN_handler();
+      void CMD_GAME_LEAVE_handler();
+      void CMD_GAME_LOAD_LAST_handler();
+      void CMD_GAME_LOAD_handler();
+      void CMD_GAME_SAVE_handler();
+      void CMD_GAME_SHOW_STATS_handler();
+      void CMD_SET_NICK_handler();
+      void CMD_EXIT_handler();
+      void CMD_HELP_handler();
+      void CMD_ERROR_INPUT_STREAM_handler();
+
+      // // // // // // // // // // //
+
+      void MSG_HELLO_handler();
+      void MSG_LOAD_DATA_handler();
+      void MSG_GAMEs_DATA_handler();
+      void MSG_PLAYER_JOINED_handler();
+      void MSG_PLAYER_LEFT_handler();
+      void MSG_PLAYER_KILLED_handler();
+      void MSG_PLAYER_GAME_OVER_handler();
+      void MSG_PLAYER_WIN_handler();
+      void MSG_GAME_RESTARTED_handler();
+      void MSG_GAME_TERMINATED_handler();
 
     public:
       mediator(client::settings_tuple &settings);
