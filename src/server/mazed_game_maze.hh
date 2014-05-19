c46b09bd505dfcb845517cc99c6a824707377cd1
Dee'Kej <deekej@linuxmail.org>
2014-05-19 21:31:01 +0200

    server: file chassis for game objects added.

---
+++ src/server/mazed_game_maze.hh
@@ -0,0 +1,99 @@
+/**
+ * @file      mazed_game_maze.hh
+ * @author    Dee'Kej (David Kaspar - xkaspa34)
+ * @version   0.1
+ * @brief     Contains a declaration of a game::maze class used on server-side.
+ */
+
+
+/* ****************************************************************************************************************** *
+ * ***[ START OF MAZED_GAME_MAZE.HH ]******************************************************************************** *
+ * ****************************************************************************************************************** */
+
+#ifndef H_GUARD_MAZED_GAME_MAZE_HH
+#define H_GUARD_MAZED_GAME_MAZE_HH
+
+
+/* ****************************************************************************************************************** *
+ ~ ~~~[ HEADER FILES ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~
+ * ****************************************************************************************************************** */
+
+#include <array>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include <boost/numeric/ublas/matrix.hpp>
+
+#include "mazed_game_globals.hh"
+#include "mazed_game_block.hh"
+#include "mazed_game_guardian.hh"
+#include "mazed_game_player.hh"
+#include "mazed_game_players_list.hh"
+#include "../basic_maze.hh"
+
+
+/* ****************************************************************************************************************** *
+ ~ ~~~[ MAZE CLASS ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~
+ * ****************************************************************************************************************** */
+
+namespace mazed {
+  class client_handler;
+  class mazes_manager;
+}
+
+namespace game {
+
+  /**
+   * Maze class to be used by the game instance's logic.
+   */
+  class maze : public basic_maze {
+      friend class mazed::client_handler;
+      friend class mazed::mazes_manager;
+      friend class game::player;
+      friend class game::guardian;
+
+      // // // // // // // // // // //
+
+      using uchar_t = unsigned char;
+
+      boost::mutex                                              access_mutex_;
+
+      long                                                      game_speed_;
+      std::string                                               game_owner_;
+      std::string                                               maze_scheme_;
+
+      game::players_list                                        players_;
+      std::unordered_map<std::string, uchar_t>                  previous_players_;
+
+      std::array<std::pair<uchar_t, uchar_t>, GAME_MAX_PLAYERS> starting_coords_;
+      std::array<std::pair<uchar_t, uchar_t>, GAME_MAX_PLAYERS> saved_coords_;
+      
+      std::vector<game::guardian>                               guardians_;
+      std::vector<game::block *>                                gates_;
+
+      boost::numeric::ublas::matrix<game::block>                matrix_;
+
+      // // // // // // // // // // //
+      
+    public:
+      maze(uchar_t row_num, uchar_t col_num, std::string &scheme) :
+        basic_maze(row_num, col_num),
+        maze_scheme_{scheme}, matrix_(row_num, col_num)
+      {{{
+        return;
+      }}}
+
+      ~maze()
+      {{{
+        return;
+      }}}
+  };
+}
+
+/* ****************************************************************************************************************** *
+ * ***[ END OF MAZED_GAME_MAZE.HH ]********************************************************************************** *
+ * ****************************************************************************************************************** */
+
+#endif
+
---
+++ src/server/mazed_game_players_list.hh
@@ -0,0 +1,162 @@
+/**
+ * @file      mazed_game_players_list.hh
+ * @author    Dee'Kej (David Kaspar - xkaspa34)
+ * @version   1.0
+ * @brief     Contains the class encapsulation all the players in the game.
+ */
+
+
+/* ****************************************************************************************************************** *
+ * ***[ START OF MAZED_GAME_PLAYERS_LIST.HH ]************************************************************************ *
+ * ****************************************************************************************************************** */
+
+#ifndef H_GUARD_MAZED_GAME_PLAYERS_LIST_HH
+#define H_GUARD_MAZED_GAME_PLAYERS_LIST_HH
+
+
+/* ****************************************************************************************************************** *
+ ~ ~~~[ HEADER FILES ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~
+ * ****************************************************************************************************************** */
+
+#include <array>
+
+#include <boost/thread.hpp>
+
+#include <cassert>
+
+#include "mazed_game_globals.hh"
+#include "mazed_game_player.hh"
+
+
+/* ****************************************************************************************************************** *
+ ~ ~~~[ PLAYER CLASS ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~
+ * ****************************************************************************************************************** */
+
+namespace game {
+
+  /**
+   * Class which acts as a std::vector, containing pointers to the game::player classes, but it also acts as a mutex,
+   * allowing locking the access to the contents of the class.
+   */
+  class players_list {
+      std::array<player *, GAME_MAX_PLAYERS>      players_;
+      unsigned char                               first_empty_ {0};
+      unsigned char                               used_slots_ {0};
+      boost::upgrade_mutex                        access_mutex_;
+
+    public:
+      players_list()
+      {{{
+        players_.fill(NULL);
+        return;
+      }}}
+
+
+      ~players_list()
+      {{{
+        return;
+      }}}
+
+
+      void lock()
+      {{{
+        access_mutex_.lock();
+        return;
+      }}}
+
+
+      void unlock()
+      {{{
+        access_mutex_.unlock();
+        return;
+      }}}
+
+
+      void lock_upgrade()
+      {{{
+        access_mutex_.lock_upgrade();
+        return;
+      }}}
+
+
+      void unlock_upgrade()
+      {{{
+        access_mutex_.unlock_upgrade();
+        return;
+      }}}
+
+
+      std::array<player *, GAME_MAX_PLAYERS>::iterator begin()
+      {{{
+        return players_.begin();
+      }}}
+
+
+      std::array<player *, GAME_MAX_PLAYERS>::iterator end()
+      {{{
+        return players_.end();
+      }}}
+
+
+      unsigned char get_used_slots()
+      {{{
+        return used_slots_;
+      }}}
+
+
+      unsigned char add(game::player *p_player)
+      {{{
+        unsigned char retval = first_empty_;
+        
+        if (first_empty_ < GAME_MAX_PLAYERS) {
+          players_[first_empty_++] = p_player;
+          used_slots_++;
+
+          std::array<player *, GAME_MAX_PLAYERS>::iterator it;
+
+          for (it = players_.begin() + first_empty_; it != players_.end() && *it != NULL; it++) {
+            first_empty_++;
+          }
+        }
+
+        return retval;
+      }}}
+
+
+      void remove(unsigned char player_num)
+      {{{
+        players_[player_num] = NULL;
+        used_slots_--;
+        
+        if (player_num < first_empty_) {
+          first_empty_ = player_num;
+        }
+
+        return;
+      }}}
+
+
+#ifndef NDEBUG
+      void remove(unsigned char player_num, game::player *p_player)
+      {{{
+        assert(players_[player_num] == p_player);
+        used_slots_--;
+
+        players_[player_num] = NULL;
+        
+        if (player_num < first_empty_) {
+          first_empty_ = player_num;
+        }
+
+        return;
+      }}}
+#endif
+  };
+}
+
+/* ****************************************************************************************************************** *
+ * ***[ END OF MAZED_GAME_PLAYERS_LIST.HH ]************************************************************************** *
+ * ****************************************************************************************************************** */
+
+#endif
+
